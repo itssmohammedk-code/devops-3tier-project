c8e5609f903c4375d4fecf24473f6928dc8c0ef2
mohammed khan <itssmohammedk@gmail.com>
2026-09-03 15:20:00 +0530

    Add Node.js backend

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+    res.send("DevOps 3-Tier Application Backend is running!");
+});
+
+app.get("/health", (req, res) => {
+    res.json({
+        status: "healthy"
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Backend server running on port ${PORT}`);
+});
